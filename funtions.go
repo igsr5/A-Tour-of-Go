f727c1d9af27d7eebe17f4a0ccb804583bb67577
Sora Ichigo <ichigo@web-freework.com>
2021-01-16 00:39:12 +0900

    add swap

--- funtions.go
+++ funtions.go
@@ -6,7 +6,13 @@ func add(x, y int) int {
   return x + y
 }
 
+func swap(x, y string) (string, string) {
+  return y, x
+}
+
 func main() {
   fmt.Println("hello world!")
   fmt.Println(add(42, 13))
+  a, b := swap("hello", "world")
+  fmt.Println(a, b)
 }
