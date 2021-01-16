0d4a444a7b15d05bc77f848ebec9f3db21d313ac
Sora Ichigo <ichigo@web-freework.com>
2021-01-16 23:40:03 +0900

    update funtions.go

--- funtions.go
+++ funtions.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "math"
+//import "math"
 
 func add(x, y int) int {
   return x + y
@@ -18,22 +18,34 @@ func split(sum int) (x, y int) {
 }
 
 func main() {
-  var hello string = "hello world!"
-  fmt.Println(hello)
+  //var hello string = "hello world!"
+  //fmt.Println(hello)
 
-  fmt.Println(add(42, 13))
+  //fmt.Println(add(42, 13))
 
-  a, b := swap("hello", "world")
-  fmt.Println(a, b)
+  //a, b := swap("hello", "world")
+  //fmt.Println(a, b)
 
-  x, y := split(10)
-  fmt.Println(x,y)
+  //x, y := split(10)
+  //fmt.Println(x,y)
 
-  var x1, y1 int = 3, 4
-  var f float64 = math.Sqrt(float64(x1*x1 + y1*y1))
-  var z uint = uint(f)
-  fmt.Println(x1, y1, z)
+  //var x1, y1 int = 3, 4
+  //var f float64 = math.Sqrt(float64(x1*x1 + y1*y1))
+  //var z uint = uint(f)
+  //fmt.Println(x1, y1, z)
 
-  const PI = 3.14
-  fmt.Println(PI)
+  //const PI = 1 << 1
+  //fmt.Println(PI)
+
+  for i := 0; i < 10; i++ {
+    fmt.Println(i)
+    if i % 2 == 0 {
+      fmt.Println("even")
+    }
+  }
+  //sum := 1
+  //for sum < 100 {
+  //  sum +=2
+  //  fmt.Println(sum)
+  //}
 }
