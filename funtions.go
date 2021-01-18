c0518aa7e149e493c8c09f8296719967492c8213
Sora Ichigo <ichigo@web-freework.com>
2021-01-18 22:03:52 +0900

    update functions.go

--- funtions.go
+++ funtions.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "time"
+//import "time"
 //import "math"
 
 func add(x, y int) int {
@@ -58,19 +58,52 @@ func main() {
   //  fmt.Println(sum)
   //}
 
-  sqrt := sqrt(81)
-  defer fmt.Println(sqrt)
+  //sqrt := sqrt(81)
+  //defer fmt.Println(sqrt)
 
-  fmt.Println("When's Saturday?")
-	today := time.Now().Weekday()
-	switch time.Saturday {
-	case today + 0:
-		fmt.Println("Today.")
-	case today + 1:
-		fmt.Println("Tomorrow.")
-	case today + 2:
-		fmt.Println("In two days.")
-	default:
-		fmt.Println("Too far away.")
-	}
+  //fmt.Println("When's Saturday?")
+	//today := time.Now().Weekday()
+	//switch time.Saturday {
+	//case today + 0:
+	//	fmt.Println("Today.")
+	//case today + 1:
+	//	fmt.Println("Tomorrow.")
+	//case today + 2:
+	//	fmt.Println("In two days.")
+	//default:
+	//	fmt.Println("Too far away.")
+	//}
+  //i, j := 42, 2701
+  //p := &i
+  //fmt.Println(*p)
+  //*p = 21
+  //fmt.Println(i)
+
+  //p = &j
+  //*p = *p / 37
+  //fmt.Println(j)
+
+  //type Vertex struct {
+  //  X int
+  //  Y int
+  //}
+
+  //v := &Vertex{1, 2}
+  //v.X = 4
+  //fmt.Println(v.X, v.Y)
+   names := [4]string{
+     "John",
+     "Paul",
+     "George",
+     "Ringo",
+   }
+   fmt.Println(names)
+  
+   a := names[0:2]
+   b := names[1:3]
+   fmt.Println(a, b)
+
+   b[0] = "XXX"
+   fmt.Println(a, b)
+   fmt.Println(names)
 }
